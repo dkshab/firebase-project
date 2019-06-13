5eb649ceaa95b7bc0085f664dd8acee70dcc088a
Dumisani <dumisanik.shabangu@gmail.com>
2019-06-13 18:43:51 +0200

    uploading a profile picture

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,14 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import "firebase/storage";
 
 var firebaseConfig = {
   apiKey: "AIzaSyDJyHTRZPRMMOunw92x3Xk09GbzXpHO2k8",
   authDomain: "fir-project-67e88.firebaseapp.com",
   databaseURL: "https://fir-project-67e88.firebaseio.com",
   projectId: "fir-project-67e88",
-  storageBucket: "",
+  storageBucket: "fir-project-67e88.appspot.com",
   messagingSenderId: "1093906003913",
   appId: "1:1093906003913:web:dda995e763317fd4"
 };
@@ -16,6 +17,7 @@ firebase.initializeApp(firebaseConfig);
 
 export const firestore = firebase.firestore();
 export const auth = firebase.auth();
+export const storage = firebase.storage();
 
 export const provider = new firebase.auth.GoogleAuthProvider();
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
