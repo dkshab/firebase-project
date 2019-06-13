eb4a13f742fe9f476074f37f93731c66ae06a27d
Dumisani <dumisanik.shabangu@gmail.com>
2019-06-13 18:58:37 +0200

    document routing

---
+++ src/components/PostPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class PostPage extends Component {
+  state = { post: null, comments: [] };
+
+  render() {
+    return <div>Post Page!</div>;
+  }
+}
+
+export default PostPage;
